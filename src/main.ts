9be47f6b05212f19186c460ad327570701896ad9
xigua <hhbuhuiai@163.com>
2023-04-27 23:58:27 +0800

    webrtc demo

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,33 @@
-import Quill from "quill";
-import QuillCursors from 'quill-cursors'
-import 'quill/dist/quill.snow.css'
-import { QuillBinding } from "y-quill";
+import Quill from 'quill';
+import QuillCursors from 'quill-cursors';
+import 'quill/dist/quill.snow.css';
+import { QuillBinding } from 'y-quill';
+import { WebrtcProvider } from 'y-webrtc';
 
 import * as Y from 'yjs';
 
-Quill.register('modules/cursor', QuillCursors);
+Quill.register('modules/cursors', QuillCursors);
 
 const quill = new Quill(document.querySelector('#editor')!, {
   modules: {
     cursors: true,
     toolbar: [
-      [{ header: [1, 2, false] }], 
+      [{ header: [1, 2, false] }],
       ['bold', 'italic', 'underline'],
-      ['image', 'code-block']
+      ['image', 'code-block'],
     ],
     history: {
-      userOnly: true
-    }
+      userOnly: true,
+    },
   },
   placeholder: '前端西瓜哥...',
   theme: 'snow',
-})
+});
 
+const ydoc = new Y.Doc(); // yjs 文档，保存共享数据
+const ytext = ydoc.getText('quill');
 
-const ydoc = new Y.Doc() // yjs 文档，保存共享数据
+const provider = new WebrtcProvider('quill-demo-room', ydoc);
 
-const ytext = ydoc.getText('quill') // 定一个鹅哥共享文本类型？
-
-const binding = new QuillBinding(ytext, quill);
-
-
-
+// 绑定
+const binding = new QuillBinding(ytext, quill, provider.awareness);
