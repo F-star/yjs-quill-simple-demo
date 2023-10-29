09eb01846d112fd236e0d857bb8048ed34573f73
xigua <hhbuhuiai@163.com>
2023-10-29 13:21:27 +0800

    feat: 简单 demo

---
+++ README.md
@@ -0,0 +1,7 @@
+启动
+
+```sh
+pnpm install
+HOST=localhost PORT=1234 npx y-websocket
+pnpm dev
+```
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import Quill from 'quill';
 import QuillCursors from 'quill-cursors';
-import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.snow.css'; // 使用了 snow 主题色
+import * as Y from 'yjs';
 import { QuillBinding } from 'y-quill';
 import { WebsocketProvider } from 'y-websocket';
-import { IndexeddbPersistence } from 'y-indexeddb';
 
-import * as Y from 'yjs';
-
+// 使用 cursors 插件
 Quill.register('modules/cursors', QuillCursors);
 
-const quill = new Quill(document.querySelector('#editor')!, {
+const quill = new Quill(document.querySelector('#app')!, {
   modules: {
     cursors: true,
     toolbar: [
@@ -18,38 +17,20 @@ const quill = new Quill(document.querySelector('#editor')!, {
       ['image', 'code-block'],
     ],
     history: {
-      userOnly: true,
+      userOnly: true, // 用户自己实现历史记录
     },
   },
   placeholder: '前端西瓜哥...',
   theme: 'snow',
 });
 
-const ydoc = new Y.Doc(); // yjs 文档，保存共享数据
-const ytext = ydoc.getText('quill');
-
-const roomName = 'quill-demo-room';
-
-const persistence = new IndexeddbPersistence(roomName, ydoc);
-
-persistence.on('synced', () => {
-  console.log('同步内容到本地');
-});
-
-const provider = new WebsocketProvider('wss://demos.yjs.dev', roomName, ydoc);
-
-const awareness = provider.awareness;
-// 绑定
-const binding = new QuillBinding(ytext, quill, awareness);
-
-// 感知对象的变化（这里指的是光标）
-awareness.on('change', () => {
-  console.log(Array.from(awareness.getStates().values()));
-});
-
-// 给要进行传播的 awareness 对象加上一个 user 字段
-// 这个字段会通过 QuillBinding 加到虚拟光标的末尾。
-awareness.setLocalStateField('user', {
-  name: '西瓜',
-  color: '#f04',
-});
+const ydoc = new Y.Doc(); // y 文档对象，保存需要共享的数据
+const ytext = ydoc.getText('quill'); // 创建名为 quill 的 Text 对象
+// 连接到 websocket 服务端
+const provider = new WebsocketProvider(
+  'ws://localhost:1234',
+  'quill-demo-room',
+  ydoc
+);
+// 数据模型绑定，再绑上光标对象
+const binding = new QuillBinding(ytext, quill, provider.awareness);
